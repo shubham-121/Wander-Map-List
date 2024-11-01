f0bbc4b961a9726f59e0ad366c8ce115013b0f83
shubham-121 <shubhambhatt121@gmail.com>
2024-11-01 19:15:59 +0530

    added about us  component and  css

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Homepage from "./components/Homepage.jsx";
+import Homepage from "./components/Homepage/Homepage.jsx";
+import AboutUs from "./components/AboutUs/AboutUs.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Homepage></Homepage>
+    {/* <Homepage></Homepage> */}
+    <AboutUs></AboutUs>
   </StrictMode>
 );
