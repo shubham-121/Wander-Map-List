90b2f320167988e6dcac5b3c2951cceba666e962
shubham-121 <shubhambhatt121@gmail.com>
2024-11-03 14:17:04 +0530

    Updated the form validation using context api and added new styles

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,23 +1,127 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import "./contactus.css";
+import facebook from "./facebook.png";
+import twitter from "./twitter.png";
 import { useReducer } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 
+const contact_state = {
+  isActive: false,
+  name: "",
+  nameArray: [],
+  phoneNum: "",
+  numberArray: [],
+  email: "",
+  emailArray: [],
+  users: [],
+  id: 0,
+};
+const ContactContext = createContext();
+
+// function handleName(e) {
+//   dispatch({ type: "Contact/Name", payload: e.target.value });
+// }
+// function handlePhoneNumber(e) {
+//   dispatch({ type: "Contact/PhoneNumber", payload: e.target.value });
+// }
+// function handleEmail(e) {
+//   dispatch({ type: "Contact/Email", payload: e.target.value });
+// }
+// function handleFormSubmit() {
+//   dispatch({ type: "Contact/FormSubmit" });
+// }
+// function handleClearForm() {
+//   dispatch({ type: "Contact/ClearForm" });
+// }
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "Contact/ToggleContactForm":
+      return {
+        ...state,
+        isActive: !state.isActive,
+      };
+    case "Contact/Name":
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case "Contact/Email":
+      return {
+        ...state,
+        email: action.payload,
+      };
+    case "Contact/PhoneNumber":
+      return {
+        ...state,
+        phoneNum: action.payload,
+      };
+    case "Contact/FormSubmit":
+      if (!state.name) {
+        //form validation
+        alert("Please enter valid name");
+        return state;
+      } else if (!state.email) {
+        alert("Please enter valid email");
+        return state;
+      } else if (!state.phoneNum) {
+        alert("Please enter valid phone number");
+        return state;
+      }
+      const newUser = {
+        name: state.name,
+        phoneNum: state.phoneNum,
+        email: state.email,
+        id: Date.now(), //uniquely identify each user
+      };
+
+      console.log("Form data", newUser);
+      console.log("Exsisting user", state.users);
+      return {
+        ...state,
+        users: [...state.users, newUser],
+        nameArray: [...state.nameArray, state.name],
+        numberArray: [...state.numberArray, state.phoneNum],
+        emailArray: [...state.emailArray, state.email],
+        name: "",
+        email: "",
+        phoneNum: "",
+      };
+    case "Contact/ClearForm":
+      console.log("Form cleared");
+      return {
+        ...state,
+        name: "",
+        phoneNum: "",
+        email: "",
+      };
+
+    default:
+      return state;
+  }
+}
+
 export default function ContactUs() {
-  const [isActive, setIsActive] = useState(false);
+  const [state, dispatch] = useReducer(reducer, contact_state);
+  const { isActive, name } = state;
 
   return (
-    <div>
-      <Header></Header>
-      <MiddleBody isActive={isActive} setIsActive={setIsActive}></MiddleBody>
-      <ContactForm isActive={isActive} setIsActive={setIsActive}></ContactForm>
-      <Footer></Footer>
-    </div>
+    <ContactContext.Provider value={{ state, dispatch }}>
+      <div>
+        <Header></Header>
+        <MiddleBody></MiddleBody>
+        <ContactForm></ContactForm>
+        <Footer></Footer>
+      </div>
+    </ContactContext.Provider>
   );
 }
 
 function Header() {
+  const { state, dispatch } = useContext(ContactContext);
+  const { isActive, name } = state;
+
   return (
     <div className="header">
       <button className="logo-btn"></button>
@@ -35,24 +139,28 @@ function Header() {
   );
 }
 
-function MiddleBody({ isActive, setIsActive }) {
-  function handleForm() {
-    setIsActive((prev) => !prev);
-  }
+function MiddleBody() {
+  const { state, dispatch } = useContext(ContactContext);
+  const { isActive, name } = state;
 
+  function toggleContactForm() {
+    dispatch({ type: "Contact/ToggleContactForm" });
+  }
   return (
     <div className="middlebody">
       <p>Wanna connect or have any queries? Straight away contact us!</p>{" "}
-      <button onClick={handleForm} className="middlebody-btn">
+      <button className="middlebody-btn" onClick={toggleContactForm}>
         ContactUs
       </button>
     </div>
   );
 }
 
-function ContactForm({ isActive, setIsActive }) {
+function ContactForm() {
+  const { state, dispatch } = useContext(ContactContext);
+  const { isActive, name } = state;
   return (
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={(e) => e.preventDefault()}>
       <div className="bg-text">Turn Dreams into Destinations.</div>
 
       {/* toggle contact form on and off */}
@@ -61,72 +169,88 @@ function ContactForm({ isActive, setIsActive }) {
   );
 }
 
-function DisplayForm({ isActive, setIsActive }) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phonerNum, setPhoneNum] = useState(0);
+function DisplayForm() {
+  const { state, dispatch } = useContext(ContactContext);
+  const { isActive, name, phoneNum, email } = state;
 
-  function handleForm() {
-    setIsActive((prev) => !prev);
+  function toggleContactForm() {
+    dispatch({ type: "Contact/ToggleContactForm" });
   }
 
   function handleName(e) {
-    setName(e.target.value);
-  }
-
-  function handleEmail(e) {
-    setEmail(e.target.value);
+    if (!e.target.value) alert("Please enter a valid name");
+    dispatch({ type: "Contact/Name", payload: e.target.value });
   }
   function handlePhoneNumber(e) {
-    setPhoneNum(e.target.value);
+    if (!e.target.value) alert("Please enter a valid phone number");
+
+    dispatch({ type: "Contact/PhoneNumber", payload: e.target.value });
+  }
+  function handleEmail(e) {
+    if (!e.target.value) alert("Please enter a valid email");
+
+    dispatch({ type: "Contact/Email", payload: e.target.value });
+  }
+  function handleFormSubmit() {
+    dispatch({ type: "Contact/FormSubmit" });
+  }
+  function handleClearForm() {
+    dispatch({ type: "Contact/ClearForm" });
   }
 
   return (
     <div className="form-container">
       <div className="form-group">
-        <button onClick={handleForm} className="close-btn">
+        <button className="close-btn" onClick={toggleContactForm}>
           X
         </button>
         <span className="label"> Full Name:</span>
         <input
-          value={name}
-          onChange={handleName}
+          id="nameinput"
           className="inputs"
           type="text"
           placeholder="Enter The Full Name"
+          value={name}
+          onChange={handleName}
         ></input>
       </div>
 
       <div className="form-group">
-        <span className="label" value={name} onChange={handleName}>
-          Email:
-        </span>
+        <span className="label">Email:</span>
 
         <input
           className="inputs"
           type="text"
           placeholder="Enter The Email"
+          value={email}
+          onChange={handleEmail}
         ></input>
       </div>
       <div className="form-group">
-        <span className="label" value={name} onChange={handleName}>
-          Contact Number:
-        </span>
+        <span className="label">Contact Number:</span>
 
         <input
           className="inputs"
-          type="text="
+          type="text"
+          value={phoneNum}
           placeholder="Enter The Contact Number"
+          onChange={handlePhoneNumber}
         ></input>
       </div>
       <div className="btn-container">
-        <button className="form-btn">Submit</button>
-        <button className="form-btn">Clear</button>
+        <button className="form-btn" onClick={handleFormSubmit}>
+          Submit
+        </button>
+        <button className="form-btn" onClick={handleClearForm}>
+          Clear
+        </button>
       </div>
     </div>
   );
 }
 function Footer() {
+  const { state, dispatch } = useContext(ContactContext);
+  const { isActive, name } = state;
   return (
     <div className="footer">
       <div className="footer-content">
@@ -159,12 +283,13 @@ function Footer() {
           </p>
         </div>
 
+        <div>Follow us at:</div>
         <div className="footer-socials">
           <a href="https://facebook.com">
-            <img src="facebook-icon.png" alt="Facebook" />
+            <img src={facebook} alt="Facebook" />
           </a>
           <a href="https://twitter.com">
-            <img src="twitter-icon.png" alt="Twitter" />
+            <img src={twitter} alt="Twitter" />
           </a>
         </div>
         <div className="footer-copyright">
